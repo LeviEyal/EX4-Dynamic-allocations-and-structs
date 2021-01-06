6db08cfe1b527771da76dd2b53c701e0b1d1aabe
Your Name <happyeyal@gmail.com>
2021-01-06 10:09:38 +0200

    changing print trie functions

--- Trie.c
+++ Trie.c
@@ -1,3 +1,10 @@
+
+/****************************************************************************
+* Eyal Levi ID.203249073
+* Assignment number 4
+* https://github.com/LeviEyal
+****************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,10 +17,10 @@ typedef struct Node{
     char letter;
     int ends;
     struct Node* children[NUM_LETTERS];
-    char* word;
+    int pos;
 } Node;
 
-
+//=============================== Adding new Node ====================================
 Node* newNode(char c){
     Node* p = (Node*)malloc(sizeof(Node));
     if(p == NULL){
@@ -24,71 +31,90 @@ Node* newNode(char c){
     return p;
 }
 
+//=============================== Adding new word ====================================
 void addWord(Node* root, char* s){
-    char* word = s;
+    int i = -1;
     while(*s){
         char current = tolower(*s);
-        if(root->children[current-'a'] == NULL)
+        if(root->children[current-'a'] == NULL){
             root->children[current-'a'] = newNode(current);
+            root->pos = i;
+        }
         root = root->children[current-'a'];
         s++;
+        i++;
         if(!*s){
             root->ends++;
-            root->word = word;
+            root->pos = i;
         }
     }
 }
 
-void printTrie(Node* root){
+//========================= Searching for word in the trie ===========================
+
+// int searchWord(char* s){
+
+// }
+
+//================================== Print trie ======================================
+void print_trie(Node* root, char *str){
     if(root == NULL) return;
+    str[root->pos] = root->letter;
     if(root->ends){
-        printf("%s\t%d\n", root->word, root->ends);
+        str[root->pos + 1] = 0;
+        printf("%s\t%d\n", str, root->ends);
     }
     for (int i = 0; i < NUM_LETTERS; i++){
-        printTrie(root->children[i]);
+        print_trie(root->children[i], str);
     }  
 }
+void printTrie(Node* root){
+    char str[WORD];
+    print_trie(root, str);
+}
 
-void printTrie_r(Node* root){
+//============================= Print trie reversed ==================================
+void print_trie_r(Node* root, char *str){
     if(root == NULL) return;
+    str[root->pos] = root->letter;
     for (int i = NUM_LETTERS-1 ; i >= 0; i--){
-        printTrie_r(root->children[i]);
+        print_trie_r(root->children[i], str);
     }  
     if(root->ends){
-        printf("%s\t%d\n", root->word, root->ends);
+        str[root->pos + 1] = 0;
+        printf("%s\t%d\n", str, root->ends);
     }
 }
+void printTrie_r(Node* root){
+    char str[WORD];
+    print_trie_r(root, str);
+}
 
+//=============================== Free trie memory ====================================
 void freeTrie(Node* root){
     if(root == NULL) return;
-    if(root->ends){
-        free(root->word);
-    }
     for (int i = 0; i < NUM_LETTERS; i++){
         freeTrie(root->children[i]);
     }
     free(root);
 }
 
+//===================================== MAIN ==========================================
 int main(int argc, char const *argv[])
 {
     Node* root = newNode(0);
     char c;
     int i = 0;
-    char* str = (char*)malloc(WORD);
-    while((c = getchar()) != EOF)
+    char str[WORD];
+    while(1)
     {
-        if(isspace(c)){
+        c = getchar();
+        if(isspace(c) || c == EOF){
             str[i] = 0;
             addWord(root, str);
-            if(c == '\n')
+            if(c == '\n' || c == EOF)
                 break;
             i=0;
-            str = (char*)malloc(WORD);
-            if(str == NULL){
-                printf("ERROR");
-                exit(1);
-            }
         }
         else if(isalpha(c)){
             c = tolower(c);
